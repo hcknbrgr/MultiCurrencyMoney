5897e69f8e71f241826146727b88262cc58e64af
hcknbrgr <hcknbrgr@gmail.com>
2025-07-10 21:26:04 -0400

    Introduce a new test for reducing the sum

--- src/AppTest.java
+++ src/AppTest.java
@@ -44,6 +44,14 @@ public class AppTest {
     }
 
     @Test
+    public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+    }
+
+    @Test
     public void testPlusReturnsSum() {
         Money five = Money.dollar(5);
         Expression result = five.plus(five);

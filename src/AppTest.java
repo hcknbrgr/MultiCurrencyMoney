17efba98f61f05903a8f936a0d761953b909e9a9
hcknbrgr <hcknbrgr@gmail.com>
2025-07-10 20:59:42 -0400

    Failing test for simple money addition

--- src/AppTest.java
+++ src/AppTest.java
@@ -34,4 +34,10 @@ public class AppTest {
         assertEquals("CHF", Money.franc(1).currency());
     }
 
+    @Test
+    public void testSimpleAddition() {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
+
 }

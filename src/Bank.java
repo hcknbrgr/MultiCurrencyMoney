cbc1ad859f9761a2b3e0f39f5a0af78b734311a4
hcknbrgr <hcknbrgr@gmail.com>
2025-07-14 18:07:50 -0400

    Passing test for adding and reducing money of different currencies

--- src/Bank.java
+++ src/Bank.java
@@ -1,5 +1,19 @@
+import java.util.Hashtable;
+
 public class Bank {
-    Money reduce(Expression source, String to) {
-        return source.reduce(to);
+    private Hashtable<Pair, Integer> rates = new Hashtable<>();
+
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
     }
+
+    int rate(String from, String to) {
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
+    }
+
+    Money reduce(Expression source, String to) {
+        return source.reduce(this, to);
+    }
+
 }

7415047cf3b44c4e0e4eda3f63515e07d42c843f
hcknbrgr <hcknbrgr@gmail.com>
2025-07-15 21:57:19 -0400

    Failing test for addition of differing currencies

--- src/AppTest.java
+++ src/AppTest.java
@@ -75,4 +75,14 @@ public class AppTest {
         assertEquals(five, sum.addend);
     }
 
+    @Test
+    public void testMixedAddition() {
+        Money fiveBucks = Money.dollar(5);
+        Money tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
+
 }
